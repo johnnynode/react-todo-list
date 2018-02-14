f24e1391e27f67d6810b28f048392c31c8976b43
johnnynode <johnnynode@gmail.com>
2018-02-15 06:16:11 +0800

    adjust header component and input component

--- src/TodoHeader.js
+++ src/TodoHeader.js
@@ -10,10 +10,10 @@ class TodoHeader extends Component {
   }
 
   render() {
-    const {name, todoCount} = this.props;
+    const {name, count} = this.props;
     return (
       <div>
-        <h1> Hello {name} you have {todoCount} items left to be done!</h1>
+        <h1> Hello {name} you have {count} items left to be done!</h1>
       </div>
     )
   }
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,13 +3,17 @@ import React, {Component} from 'react';
 class TodoInput extends Component {
   static get defaultProps() {
     return {
+      type: "text",
+      style: {width:300,height:30},
+      placeholder: "please input your new todo task ...",
+      autoFocus: false,
     }
   }
 
   render() {
     console.log('I am Input');
     return (
-      <input />
+      <input {... this.props}/>
     )
   }
 }
