d0b53ee862ad0b8ef42ea7a69af64e9914c5111f
johnnynode <johnnynode@gmail.com>
2018-03-17 09:58:03 +0800

    add list component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import Header from './views/header';
 import Input from './views/input';
+import List from './views/list';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <Header />
         <Input />
+        <List />
       </div>
     )
   }
---
+++ src/views/list.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>book1</li>
+          <li>book2</li>
+          <li>book3</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default List;
