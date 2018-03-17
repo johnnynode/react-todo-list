6a778628914330d1764eda9920b811042f672d04
johnnynode <johnnynode@gmail.com>
2018-03-17 09:26:10 +0800

    finish the UI

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDom from 'react-dom';
 
 ReactDom.render(
   <div>
-    Hello World  
+    <div>
+      <h1>React's Basic Todo List</h1>
+      <div>Hello John , you have 3 books left to read!</div>
+    </div>  
+    <div>
+      <input placeholder="请添加书籍..." />
+    </div>
+    <div>
+      <ul>
+        <li>book1</li>
+        <li>book2</li>
+        <li>book3</li>
+      </ul>
+    </div>
   </div>,
   document.getElementById("root")
 )
