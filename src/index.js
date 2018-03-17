2ae5b5566fa07c2e8bf9e02a273d48ceff63005a
johnnynode <johnnynode@gmail.com>
2018-03-17 09:53:01 +0800

    add header component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import App from './app';
+
 ReactDom.render(
-  <div>
-    <div>
-      <h1>React's Basic Todo List</h1>
-      <div>Hello John , you have 3 books left to read!</div>
-    </div>  
-    <div>
-      <input placeholder="请添加书籍..." />
-    </div>
-    <div>
-      <ul>
-        <li>book1</li>
-        <li>book2</li>
-        <li>book3</li>
-      </ul>
-    </div>
-  </div>,
+  <App />,
   document.getElementById("root")
 )
---
+++ src/views/header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React's Basic Todo List</h1>
+        <div>Hello John , you have 3 books left to read!</div>
+      </div>
+    )
+  }
+}
+
+export default Header;
