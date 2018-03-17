5bc15621fe4cd57e0301f1b2d47ce4c7a2f48e2f
johnnynode <johnnynode@gmail.com>
2018-03-17 09:55:38 +0800

    add input component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 import Header from './views/header';
+import Input from './views/input';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
+        <Input />
       </div>
     )
   }
