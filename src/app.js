6deef1e3338bbb9c24d83040c40a1eb315d7b64c
johnnynode <johnnynode@gmail.com>
2018-03-20 09:40:44 +0800

    load data from componentWillMount and handle header component

--- src/app.js
+++ src/app.js
@@ -5,16 +5,40 @@ import Input from './views/input';
 import List from './views/list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookList : []
+    }
+  }
+
   render() {
+    const {
+      bookList
+    } = this.state;
+
+    let bookLeft = bookList.filter((item)=>!item.checked).length;
     return (
       <div>
-        <Header />
+        <Header name="John" bookLeft={bookLeft} />
         <Input 
           style={{width:250,height:30,outline:"none",margin:"10px 0",paddingLeft:5}} 
         />
         <List />
       </div>
     )
+  }
+
+  componentWillMount() {
+    fetch('books.json')
+    .then((data) => {
+      return data.json();
+    })
+    .then((bookList) => {
+      this.setState({
+        bookList
+      })
+    })
   }
 }
 
--- src/views/header.js
+++ src/views/header.js
@@ -4,18 +4,18 @@ class Header extends Component {
   static get defaultProps() {
     return {
       name:"Johnny",
-      leftCount:0
+      bookLeft:0
     }
   }
   render() {
     const {
       name,
-      leftCount
+      bookLeft
     } = this.props;
     return (
       <div>
         <h1>React's Basic Todo List</h1>
-        <div>Hello {name} , you have {leftCount} books left to read!</div>
+        <div>Hello {name} , you have {bookLeft} books left to read!</div>
       </div>
     )
   }
